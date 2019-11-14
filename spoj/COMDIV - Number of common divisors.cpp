6d356cf9fb21de90861e2c7e72eb452381d0a52c
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-11-14 23:15:08 +0600

    Add files via upload

---
+++ spoj/COMDIV - Number of common divisors.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,c=0,i,j,t,k,lie,m,n,o,x,y,z,ar[200010];
+    S(t);
+    while(t--){
+        SS(a,b);
+        ll s=0;
+        x=gcd(a,b);
+        for(i=1;i<=sqrt(x);i++){
+            if(x%i==0){
+                if(x/i==i) s++;
+                else s+=2;
+            }
+        }
+        printf("%lld\n",s);
+    }
+
+    return 0;
+}
