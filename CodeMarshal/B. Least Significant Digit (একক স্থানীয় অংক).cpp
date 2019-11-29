53684fcfcb0788d1df324ca93e1444b15feac154
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2019-11-29 14:46:51 +0600

    Create B. Least Significant Digit (একক স্থানীয় অংক).cpp

---
+++ CodeMarshal/B. Least Significant Digit (একক স্থানীয় অংক).cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,i,j,t,k,lie,ans=0,m,n,o,x,y,z;
+    SS(n,x);
+    m=n;
+    ans=n/10;
+    n%=10;
+    if(x<=n) ans++;
+    if(x==0) ans--;
+    printf("%lld\n",ans);
+
+    return 0;
+}
