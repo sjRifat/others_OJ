3f741c8677a697ad41609a182d02f012c99021b1
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2020-02-04 00:21:55 +0600

    Create National Flag.cpp

---
+++ toph/National Flag.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,c=0,d,sm=0,i,j,t,k,lie,m,n,o,x,y,z,zz,ar[200010];
+    SS(x,y);
+    zz=(x-y);
+    z=(x-y)/2;
+    if(zz<=0 || zz&1 || 5*z>x) printf("Not possible\n");
+    else printf("%lld %lld\n",5*z,3*z);
+
+    return 0;
+}
