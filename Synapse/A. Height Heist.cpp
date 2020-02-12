16ea44b358290b2b849c00305107b9cbd60b8f96
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2020-02-12 17:48:53 +0600

    Create A. Height Heist.cpp

---
+++ Synapse/A. Height Heist.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll a,b,c=0,i,j,t,k,lie,m,n,o,x,y,z;
+    S(t);
+    while(t--){
+        ll ar[110],sm=0,cnt=0;
+        S(n);
+        for(i=0;i<n;i++){
+            S(ar[i]);
+            sm+=ar[i];
+        }
+        sm/=n;
+        for(i=0;i<n;i++){
+            if(ar[i]>sm) cnt++;
+        }
+        printf("%lld\n",cnt);
+    }
+
+    return 0;
+}
