7cf6738ae58c6e86198bfa73898c777956002dbc
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2020-02-12 17:49:47 +0600

    Create B. Fill the Tank.cpp

---
+++ Synapse/B. Fill the Tank.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef		long long       ll;
+typedef		vector<ll>      vll;
+#define		pb              push_back
+#define		PB              pop_back
+#define		nn              "\n"
+#define		all(p)          p.begin(),p.end()
+#define		zz(v)           (ll)v.size()
+#define		mms(ar,a)       memset(ar,a,sizeof(ar))
+#define		ss              ' '
+#define 	S(a)            scanf("%lld",&a)
+#define 	SS(a,b)         scanf("%lld %lld",&a,&b)
+#define 	SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+#define		gcd(a,b)        __gcd(a,b)
+#define		lcm(a,b)        (a*b)/gcd(a,b)
+#define		pi              acos(-1.0)
+
+int main(){
+//    freopen("input.txt","r",stdin);
+    ll i,j,t,k,lie,m,n,o,x,y,z;
+    S(t);
+    while(t--){
+        ll a,b,c;
+        SSS(a,b,c);
+        x=lcm(a,b);
+        x=lcm(x,c);
+        ll sm=x/a+x/b+x/c;
+        printf("%lld\n",x/sm);
+    }
+
+    return 0;
+}
