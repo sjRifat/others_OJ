e5abf2354301f5fb1e661494841a38105dc32655
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2021-01-09 00:51:07 +0600

    jP

---
+++ CSES/Josephus Problem II.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+typedef	    long long       ll;
+typedef	    vector<ll>      vll;
+#define	    ss              ' '
+#define	    nn              "\n"
+#define	    PB              pop_back
+#define     pb              push_back
+#define	    pi              acos(-1.0)
+#define	    gcd(a,b)        __gcd(a,b)
+#define	    OO              1000000007
+#define	    NN              printf("\n")
+#define	    zz(v)           (ll)v.size()
+#define	    lcm(a,b)        (a*b)/gcd(a,b)
+#define     no              printf("NO\n")
+#define     mo              printf("-1\n")
+#define     yes             printf("YES\n")
+#define     S(a)            scanf("%lld",&a)
+#define	    all(p)          p.begin(),p.end()
+#define     P(a)            printf("%lld\n",a)
+#define     db              printf("be steady\n")
+#define	    mms(ar,a)       memset(ar,a,sizeof(ar))
+#define     SS(a,b)         scanf("%lld %lld",&a,&b)
+#define     PP(a,b)         printf("%lld %lld\n",a,b)
+#define     SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+typedef     tree<ll,null_type,less<ll>,rb_tree_tag,tree_order_statistics_node_update> ordered_set;
+
+ll ar[200010];
+void solve(){
+    ll a,b,c=1,i,j,k,m,n,o,x,y,z;
+    ll p=2,sm=0,cnt=0,mx=-OO,mn=OO;
+    ordered_set os;
+    SS(n,k);
+    for(i=1;i<=n;i++) os.insert(i);
+    i=k;
+    while(!os.empty()){
+        i%=zz(os);
+        x=*os.find_by_order(i);
+        cout<<x<<ss;
+        os.erase(x);
+        i+=k;
+    }
+
+}
+int main(){
+//	freopen("input.txt", "r", stdin);
+//    ll t;
+//    S(t);
+//    while(t--)
+        solve();
+
+    return 0;
+}
+
+
