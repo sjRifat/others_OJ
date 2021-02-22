d66acd7396e91c10fc61066168537b98318803f4
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2021-02-22 21:52:52 +0600

    dice dp

---
+++ CSES/Dice Combinations.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef	    long long       ll;
+typedef	    vector<ll>      vll;
+#define	    ss              ' '
+#define	    nn              "\n"
+#define	    fi              first
+#define	    se              second
+#define	    PB              pop_back
+#define     pb              push_back
+#define	    pi              acos(-1.0)
+#define	    gcd(a,b)        __gcd(a,b)
+#define	    OO              1000000007
+#define	    NN              printf("\n")
+#define	    zz(v)           (ll)v.size()
+#define	    lcm(a,b)        (a*b)/gcd(a,b)
+#define     no              printf("NO\n")
+#define     mo              printf("-1\n")
+#define     yes             printf("YES\n")
+#define     S(a)            scanf("%lld",&a)
+#define	    all(p)          p.begin(),p.end()
+#define     P(a)            printf("%lld\n",a)
+#define     db              printf("be steady\n")
+#define	    mms(ar,a)       memset(ar,a,sizeof(ar))
+#define     SS(a,b)         scanf("%lld %lld",&a,&b)
+#define     PP(a,b)         printf("%lld %lld\n",a,b)
+#define     SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+
+ll dp[1000010];
+void solve(){
+    ll a=0,b=0,c=0,n,i,j,k,m,o,x,y=0,z;
+    ll p=0,sm=0,cnt=0,mx=-OO,mn=OO;
+    S(n);
+    dp[0]=1;
+    for(i=1;i<=n;i++){
+        for(j=1;j<7;j++){
+            if(j>i) break;
+            dp[i]+=dp[i-j];
+            dp[i]%=OO;
+        }
+    }
+    P(dp[n]);
+}
+
+int main(){
+//	freopen("input.txt", "r", stdin);
+//    ll t;
+//    S(t);
+//    while(t--)
+        solve();
+
+    return 0;
+}
