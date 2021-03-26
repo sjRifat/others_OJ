9d0f5cd8f7f9c5a8583be0ee01421d515850c4ea
sj_rifat <45694548+sjRifat@users.noreply.github.com>
2021-03-26 21:54:08 +0600

    stl

---
+++ spoj/REDARR2 - Reduce the array.cpp
@@ -0,0 +1,59 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef	    long long       ll;
+typedef	    vector<ll>      vll;
+#define	    ss              ' '
+#define	    nn              "\n"
+#define	    fi              first
+#define	    se              second
+#define	    PB              pop_back
+#define     pb              push_back
+#define	    pi              acos(-1.0)
+#define	    gcd(a,b)        __gcd(a,b)
+#define	    OO              1000000007
+#define	    NN              printf("\n")
+#define	    zz(v)           (ll)v.size()
+#define	    lcm(a,b)        (a*b)/gcd(a,b)
+#define     no              printf("NO\n")
+#define     mo              printf("-1\n")
+#define     yes             printf("YES\n")
+#define     S(a)            scanf("%lld",&a)
+#define	    all(p)          p.begin(),p.end()
+#define     P(a)            printf("%lld\n",a)
+#define     db              printf("be steady\n")
+#define	    mms(ar,a)       memset(ar,a,sizeof(ar))
+#define     SS(a,b)         scanf("%lld %lld",&a,&b)
+#define     PP(a,b)         printf("%lld %lld\n",a,b)
+#define     SSS(a,b,c)      scanf("%lld %lld %lld",&a,&b,&c)
+
+ll ar[200010];
+void solve(){
+    ll a=1,b=0,c=0,n,i,j,k,m,o,x=1,y=0,z;
+    ll p=0,sm=0,cnt=0,mx=-OO,mn=OO;
+    priority_queue<ll>pq;
+    S(n);
+    while(n--){
+        S(a);
+        pq.push(-a);
+    }
+    while(!pq.empty()){
+        x=-pq.top();
+        pq.pop();
+        if(pq.empty()) break;
+        y=-pq.top();
+        pq.pop();
+        sm+=(x+y);
+        pq.push(-(x+y));
+    }
+    P(sm);
+}
+
+int main(){
+//	freopen("input.txt", "r", stdin);
+    ll t;
+    S(t);
+    while(t--)
+        solve();
+
+    return 0;
+}
